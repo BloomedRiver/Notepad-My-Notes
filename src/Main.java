d2f4a9a0631f105a19ee8e26f508dbbafa3b6b04
BloomedRiver <2022s19512@stu.cmb.ac.lk>
2025-09-13 22:01:05 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Set look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        SwingUtilities.invokeLater(() -> {
+            new MyNotes().setVisible(true);
+        });
+    }
+}
